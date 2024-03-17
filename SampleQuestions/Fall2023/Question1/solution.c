972f4a4d6475814720616008be9c7419fea6217f
Kyle Merritt <kyle.merritt@knights.ucf.edu>
2024-03-17 18:50:23 -0400

    added question 1 from Fall 2023

---
+++ SampleQuestions/Fall2023/Question1/solution.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Monster {
+// Details not necessary to solve the problem.
+    char name[32]; //Added this for demonstration purposes
+} Monster;
+
+typedef struct Monster_List {
+    Monster** mArray;
+    int numMonsters;
+} Monster_List;
+
+// Initializes the monster pointed to by mPtr to be the default
+// monster.
+void initializeMonster(Monster* mPtr);
+
+Monster_List* getDefaultMonsters(int n) {
+    Monster** monsters = (Monster**)malloc(sizeof(Monster*));
+
+    for (int i = 0; i < n; i++) {
+        monsters[i] = (Monster*)malloc(sizeof(Monster));
+        initializeMonster(monsters[i]);
+    }
+
+    Monster_List* result = (Monster_List*)malloc(sizeof(Monster_List));
+    result->mArray = monsters;
+    result->numMonsters = n;
+
+    return result;
+}
+
+void initializeMonster (Monster* mPtr) {
+    strcpy(mPtr->name, "Omen Killer");
+}
+
+int main (int) {
+    Monster_List* myMonsters = getDefaultMonsters(10);
+
+    for (int i = 0; i < 10; i++) {
+        printf("%s\n", myMonsters->mArray[i]->name);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        free(myMonsters->mArray[i]);
+    }
+}
