09182c226cc558ebe7431d8a206323f178a4ad0f
Kyle Merritt <ky563720@ucf.edu>
2024-06-16 16:40:41 -0400

    checking for powers of two initially

--- SampleQuestions/Spring2024/SectionD/Question3/solution.c
+++ SampleQuestions/Spring2024/SectionD/Question3/solution.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
+#include <math.h>
 int leastBitsOn(int x) { 
     int numOn = 0; 
     int cur = 0; 
     int longest = 0; 
+
+    // checking for powers of 2 first
+    if ((ceil(log2(x)) == floor(log2(x)))) {
+        return 1;
+    }
  
     // iterate for each bit in the integer
     for (int i = 0; i < 8*sizeof(int); i++) {
         // if we find an on bit
+        // Bitwise left shift works like this: 0000000001 << 8 = 0100000000
+        // Bitwise and works like this: 0100000000 & 0101010010 = 0100000000 // 0000000001 & 0101010010 = 0000000000
         if ((x&(1<<i)) != 0) {
             cur++;
             numOn++;
         } else {
             cur = 0;
         }
-
         if (cur > longest) {
             longest = cur;
         }
